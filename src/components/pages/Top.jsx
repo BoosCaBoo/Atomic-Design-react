95730248f2688e77984d70955d762c044dd3745f
BoosCaBoo <71345530+BoosCaBoo@users.noreply.github.com>
2021-06-01 19:09:21 +0900

    commit-1

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,16 @@
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
+  const onClickAdmin = () => ('管理');
+  const onClickAdmin = () => ('一般');
+
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br/>
+      <SecondaryButton>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
